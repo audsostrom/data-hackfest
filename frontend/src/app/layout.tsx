6df75d7633f15c7eede7c74e6852614aecf93797
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2024-05-17 21:16:34 -0700

    init

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,14 +1,13 @@
 'use client';
 import "~/styles/globals.css";
 
-import { SessionProvider } from 'next-auth/react';
 
 import { GeistSans } from "geist/font/sans";
 import {AppRouterCacheProvider} from "@mui/material-nextjs/v13-appRouter";
 import {ThemeProvider} from "@mui/material";
 import theme from "~/app/theme";
 
-
+import { TRPCReactProvider } from "~/trpc/react";
 
 export default function RootLayout({
   children,
@@ -18,12 +17,18 @@ export default function RootLayout({
   return (
     <html lang="en">
     <AppRouterCacheProvider>
-      <SessionProvider>
-        <ThemeProvider theme={theme}>
-          <body className={GeistSans.className}>{children}</body>
-        </ThemeProvider>
-      </SessionProvider>
+        <TRPCReactProvider>
+          <ThemeProvider theme={theme}>
+            <body className={GeistSans.className}>{children}</body>
+          </ThemeProvider>
+        </TRPCReactProvider>
     </AppRouterCacheProvider>
     </html>
   );
 }
+
+
+
+
+
+
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,43 +1,16 @@
-'use client'
 import Link from "next/link";
-import { useSession, signIn, signOut } from "next-auth/react";
 
+import { CreatePost } from "~/app/_components/create-post";
+import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
 import styles from "./index.module.css";
 
-export default function Home() {
-  const session = useSession();
-  const isLoggedIn = !!session.data;
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+  const session = await getServerAuthSession();
+
   return (
     <main className={styles.main}>
-      <ul className="flex gap-4">
-        {isLoggedIn && (
-          <>
-          <li>Logged in as {session.data.user.name}</li>
-            <li>
-              <button
-                onClick={() => {
-                  signOut().catch(console.error);
-                }}
-              >
-                Logout
-              </button>
-            </li>
-          </>
-        )}
-
-        {!isLoggedIn && (
-          <li>
-            <button
-       
-              onClick={() => {
-                signIn().catch(console.error);
-              }}
-            >
-              Login
-            </button>
-          </li>
-        )}
-      </ul>
       <div className={styles.container}>
         <h1 className={styles.title}>
           Create <span className={styles.pinkSpan}>T3</span> App
@@ -66,7 +39,47 @@ export default function Home() {
             </div>
           </Link>
         </div>
+        <div className={styles.showcaseContainer}>
+          <p className={styles.showcaseText}>
+            {hello ? hello.greeting : "Loading tRPC query..."}
+          </p>
+
+          <div className={styles.authContainer}>
+            <p className={styles.showcaseText}>
+              {session && <span>Logged in as {session.user?.name}</span>}
+            </p>
+            <Link
+              href={session ? "/api/auth/signout" : "/api/auth/signin"}
+              className={styles.loginButton}
+            >
+              {session ? "Sign out" : "Sign in"}
+            </Link>
+          </div>
+        </div>
+
+        <CrudShowcase />
       </div>
     </main>
+  );
+}
+
+async function CrudShowcase() {
+  const session = await getServerAuthSession();
+  if (!session?.user) return null;
+
+  const latestPost = await api.post.getLatest();
+
+  return (
+    <div className={styles.showcaseContainer}>
+      {latestPost ? (
+        <p className={styles.showcaseText}>
+          Your most recent post: {latestPost.name}
+        </p>
+      ) : (
+        <p className={styles.showcaseText}>You have no posts yet.</p>
+      )}
+
+      <CreatePost />
+    </div>
   );
 }
