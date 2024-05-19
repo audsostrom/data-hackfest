7ce4ec3865e0a81482183ea5c9b0df07d9b65a11
Hozay <josmchavz@gmail.com>
2024-05-19 05:01:26 -0700

    Signup page (#10)
    account

---
+++ frontend/src/app/_components/profile-card.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import React from 'react';
+import { Box, Typography, Avatar, Divider, Grid } from '@mui/material';
+import {Users} from "~/server/db/schema";
+
+export interface ProfileCardProps {
+  account: Users;
+}
+
+export default function ProfileCard({ account }: ProfileCardProps) {
+    return (
+        <Box
+            sx={{
+                borderRadius: 2,
+                overflow: 'hidden',
+                boxShadow: 3,
+                bgcolor: 'background.paper',
+                width: {
+                    xs: '100%',
+                    md: '95%',
+                }
+            }}
+        >
+            <Box
+                sx={{
+                    height: 100,
+                    backgroundImage: 'url(https://m.media-amazon.com/images/M/MV5BYzI2YmZhZTQtMWQ1OC00YTMxLWJlNzgtYjYyZGZjOWEyNmQxXkEyXkFqcGdeQWFybm8@._V1_.jpg)',
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+            />
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    p: 2,
+                    marginTop: '-50px',
+                }}
+            >
+                <Avatar sx={{ width: 92, height: 92, mb: 1, border: '6px solid white' }} src={account?.image ?? undefined} />
+                <Typography variant="h6">{account?.name}</Typography>
+                <Typography variant="body2" color="text.secondary">
+                    @{account?.username}
+                </Typography>
+            </Box>
+            <Grid container columns={13} justifyContent={'space-between'} sx={{
+                paddingX: '10px',
+                paddingBottom: 6,
+            }}>
+                <Grid item xs={4}>
+                    <Box sx={{ textAlign: 'center', p: 1 }}>
+                        <Typography variant="h6">18</Typography>
+                        <Typography variant="body2">Movies Seen</Typography>
+                    </Box>
+                </Grid>
+                <Divider orientation="vertical" variant="middle" flexItem />
+                <Grid item xs={4}>
+                    <Box sx={{ textAlign: 'center', p: 1 }}>
+                        <Typography variant="h6">18</Typography>
+                        <Typography variant="body2">Friends</Typography>
+                    </Box>
+                </Grid>
+                <Divider orientation="vertical" variant="middle" flexItem />
+                <Grid item xs={4}>
+                    <Box sx={{ textAlign: 'center', p: 1 }}>
+                        <Typography variant="h6">18</Typography>
+                        <Typography variant="body2">Bucket Lists</Typography>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
--- frontend/src/trpc/react.tsx
+++ frontend/src/trpc/react.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
+import {createTRPCClient, httpBatchLink, loggerLink, unstable_httpBatchStreamLink} from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import { type inferRouterInputs, type inferRouterOutputs } from "@trpc/server";
 import { useState } from "react";
 import SuperJSON from "superjson";
+import superjson from 'superjson';
+
 
 import { type AppRouter } from "~/server/api/root";
 
